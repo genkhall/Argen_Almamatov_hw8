d6936959658292e7a4a67622bb2d7e384d96f55b
genkhall <argenargen0805@gmail.com>
2023-03-24 16:27:57 +0600

    hw_5

--- casino.py
+++ casino.py
@@ -1,9 +1,11 @@
 from decouple import config
 import random
+
+
 def casino():
     money = config("MY_MONEY", cast=int)
     while True:
-        slot_of_win = random.randint(1,31)
+        slot_of_win = random.randint(1, 31)
         choose_slot = int(input("Choose one number : "))
         put_money = int(input("сколько поставить : "))
 
@@ -13,18 +15,19 @@ def casino():
             print("Недостаточно средств")
 
         if choose_slot == slot_of_win:
-            money += put_money *2
+            money += put_money * 2
         else:
             money -= put_money
         print(f'slot of win {slot_of_win}')
 
         want_play = input("Хотите еще сыграть? y/n")
         if want_play == "y":
-            print(f"your money :{money}")
+            print(f"your money : {money} ")
             continue
         if want_play == "n":
             print(f"your money :{money}")
             break
+
 
 if __name__ == "__main__":
     casino()
