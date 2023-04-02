7f32552465fd93b6d8e394c3f17a23c4764253f8
genkhall <127104930+genkhall@users.noreply.github.com>
2023-04-02 23:48:20 +0600

    Delete hw8.py

--- hw8.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-'''CREATE TABLE countries(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-title TEXT NOT NULL
-)'''
-
-'''INSERT INTO countries(title) VALUES ('Kyrgyzstan'),('China'),('Russia')'''
-
